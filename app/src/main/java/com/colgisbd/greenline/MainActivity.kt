ad3084b848ce37753f4c8ece84eae6c5016390ba
Ridowan Ahmed <ridowan.ahmed.dev@gmail.com>
2019-02-22 16:37:58 +0600

    main_menue added

--- app/src/main/java/com/colgisbd/greenline/MainActivity.kt
+++ app/src/main/java/com/colgisbd/greenline/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.colgisbd.greenline
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import android.widget.Toast.makeText
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    public fun loginClick(view: View) {
+//        makeText(this, "Login Clicked", Toast.LENGTH_LONG).show()
+        startActivity(Intent(this@MainActivity, main_menu::class.java))
     }
 }
